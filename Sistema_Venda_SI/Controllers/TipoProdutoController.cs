93e57dabc7fc6f2042ec7d45d9565c0c982cd337
Guilherme Monte-Mor <guimontemor777@gmail.com>
2023-08-09 20:58:25 -0300

    Add project files.

---
+++ Sistema_Venda_SI/Controllers/TipoProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_Venda_SI.Model.Models;
+
+namespace Sistema_Venda_SI.Controllers
+{
+    public class TipoProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(TipoProduto tipoProduto) 
+        {
+            return View(tipoProduto);
+        }
+    }
+}
