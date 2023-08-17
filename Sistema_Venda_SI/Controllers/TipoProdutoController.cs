370212fd05acc2cb7aae59f8328f4b227731cc64
Guilherme Monte-Mor <guimontemor777@gmail.com>
2023-08-16 21:04:39 -0300

    Added Tipo Produto Controller and Views

--- Sistema_Venda_SI/Controllers/TipoProdutoController.cs
+++ Sistema_Venda_SI/Controllers/TipoProdutoController.cs
@@ -12,27 +12,78 @@ namespace Sistema_Venda_SI.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+    public IActionResult Index()
         {
-            return View();
+            var ListaTipoProduto = _context.TipoProduto.ToList();
+
+            return View(ListaTipoProduto);
+        }
+
+        public IActionResult Details(int id) 
+        {
+            TipoProduto tipoProduto = new TipoProduto();
+            tipoProduto = _context.TipoProduto.FirstOrDefault(x => x.TipCodigo == id);
+            return View(tipoProduto);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var tipoProduto = _context.TipoProduto.FirstOrDefault(x =>x.TipCodigo == id);
+            return View(tipoProduto);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(TipoProduto tipoProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry<TipoProduto>(tipoProduto).State = Microsoft.EntityFrameworkCore.EntityState.Modified; 
+                _context.SaveChanges();
+                return View(tipoProduto);
+            }
+            ViewData["MensagemErro"] = "Erro no sistema";
+                return View(tipoProduto);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TipoProduto tipoProduto) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Entry(tipoProduto).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                _context.SaveChanges();
+                return View(tipoProduto);
+            }
+            ViewData["MensagemErro"] = "Erro no sistema";
+            return View(tipoProduto);
         }
 
         [HttpGet]
         public IActionResult Create() 
         {
-            return View();
+            return View();  
         }
 
-        [HttpPost]
 
-        public IActionResult Create(TipoProduto tipoProduto) 
+
+      
+        public IActionResult Delete(int id)
         {
-            if (ModelState.IsValid)
+            var tipoProduto = _context.TipoProduto.FirstOrDefault(x => x.TipCodigo == id);
+
+            if (tipoProduto != null)
             {
-                _context.Entry(tipoProduto).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                _context.Entry(tipoProduto).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
                 _context.SaveChanges();
             }
+            else
+            {
+                ViewData["MensagemErro"] = "Tipo de pagamento não encontrado.";
+            }
+
             return View(tipoProduto);
         }
+
     }
 }
